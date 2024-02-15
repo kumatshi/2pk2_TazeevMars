2343c6d1bb3e820d38ffab7484d505112d45dfa5
kumatshi <144018921+kumatshi@users.noreply.github.com>
2024-02-15 09:56:46 +0500

    Add files via upload

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace pz_18
+{
+    // Перечисление для представления типов транспорта
+    public enum TransportType { Trolleybus, Bus, Train }
+
+    // Класс, представляющий общественный транспорт
+    public class PublicTransport
+    {
+        private static int totalTransportCount = 0;
+
+        // Свойства транспорта
+        public TransportType TransportType { get; set; }
+        public string LicensePlate { get; set; }
+        public string AssignedDriver { get; set; }
+        public TimeOnly DepartureTime { get; set; }
+        public TimeOnly EndOfWorkTime { get; set; }
+
+        // Конструктор класса PublicTransport
+        public PublicTransport(TransportType type, string licensePlate, string assignedDriver, TimeOnly departureTime, TimeOnly endOfWorkTime)
+        {
+            TransportType = type;
+            // Валидация номера лицензии
+            LicensePlate = IsValidLicensePlate(licensePlate) ? licensePlate : throw new ArgumentException("Invalid license plate format");
+            AssignedDriver = string.IsNullOrEmpty(assignedDriver) ? "не назначен" : assignedDriver;
+            DepartureTime = departureTime;
+            EndOfWorkTime = endOfWorkTime;
+
+            totalTransportCount++;
+        }
+
+        // Приватный метод для валидации номера лицензии
+        private bool IsValidLicensePlate(string licensePlate) => true;
+
+        // Метод определения нахождения на маршруте
+        public bool IsOnRoute() => TimeOnly.FromDateTime(DateTime.Now) >= DepartureTime && TimeOnly.FromDateTime(DateTime.Now) <= EndOfWorkTime;
+
+        // Метод отображения информации о транспорте
+        public void DisplayInfo() => Console.WriteLine($"Госномер: {LicensePlate}, Водитель: {AssignedDriver}, Тип транспорта: {TransportType}, Состояние: {(IsOnRoute() ? "в рейсе" : "в парке")}");
+
+        // Статический метод для отображения общего количества транспортных средств
+        public static void DisplayTotalTransportCount() => Console.WriteLine($"Общее количество транспортных средств: {totalTransportCount}");
+    }
+}
---
+++ ConsoleApp4/ConsoleApp4/TransportSystem.cs
@@ -0,0 +1,30 @@
+﻿using pz_18;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransportSystem
+{
+    class TransportSystem
+    {
+        // Точка входа в программу
+        static void Main()
+        {
+            // Создание объектов и демонстрация логики работы
+            var trolleybus = new PublicTransport(TransportType.Trolleybus, "AB 123 CD", "Иванов Иван", new TimeOnly(8, 0), new TimeOnly(18, 0));
+            var bus = new PublicTransport(TransportType.Bus, "XY 456 ZA", "", new TimeOnly(9, 30), new TimeOnly(19, 30));
+            var train = new PublicTransport(TransportType.Train, "KL 789 MN", "Петров Петр", new TimeOnly(7, 45), new TimeOnly(17, 45));
+
+            // Вывод информации по каждому объекту
+            trolleybus.DisplayInfo();
+            bus.DisplayInfo();
+            train.DisplayInfo();
+
+            // Проверка нахождения на маршруте и вывод общего количества созданных объектов
+            Console.WriteLine($"Троллейбус на рейсе: {trolleybus.IsOnRoute()}");
+            PublicTransport.DisplayTotalTransportCount();
+        }
+    }
+}
